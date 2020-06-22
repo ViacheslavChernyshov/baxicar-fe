61de0d8d8bf2ae3a04ad4110cfd2b4165151def6
User <chernyshov.viacheslav@gmail.com>
2020-06-23 01:19:59 +0300

    add: - autocomplete origin and destination addresses - setup origin and destination markers

--- src/app/system/driver-page/driver-page.component.ts
+++ src/app/system/driver-page/driver-page.component.ts
@@ -1,4 +1,5 @@
 import {AfterContentInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 // import { } from '@types/googlemaps';
 
@@ -10,63 +11,120 @@ import {AfterContentInit, Component, ElementRef, ViewChild} from '@angular/core'
 
 export class DriverPageComponent implements AfterContentInit {
   @ViewChild('googleMap', {static: true}) public gMapElement: ElementRef;
+
+  @ViewChild('startAddress', {static: true}) startAddress: ElementRef;
+
+  @ViewChild('endAddress', {static: true}) endAddress: ElementRef;
+
+  @ViewChild('destinationAddress', {static: true}) destinationAddress: ElementRef;
+
+  public searchControl: FormControl;
+
   map: google.maps.Map;
-  startAddress = '';
+  startRouteMarker: google.maps.Marker;
+  endRouteMarker: google.maps.Marker;
+
+  startAddressAutocomplete: google.maps.places.Autocomplete;
+  endAddressAutocomplete: google.maps.places.Autocomplete;
 
   public ngAfterContentInit(): void {
-    const mapProperty = {
+    this.searchControl = new FormControl();
+
+    this.initMap();
+    this.initPlaces();
+  }
+
+  initMap() {
+    const mapOptions = {
       center: new google.maps.LatLng(50.4501, 30.5234),
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    this.map = new google.maps.Map(this.gMapElement.nativeElement, mapProperty);
 
-    // var autocomplete = new google.maps.places.Autocomplete(this.startAddress);
-    // autocomplete.bindTo('bounds', map);
-    //
-    // autocomplete.addListener('place_changed', function () {
-    //   if (!startRouteMarker) {
-    //     startRouteMarker = new google.maps.Marker({
-    //       map: map,
-    //       draggable: true
-    //     });
-    //   }
-    //   var place = autocomplete.getPlace();
-    //   if (!place.geometry) {
-    //     window.alert('No details available for input: \'' + place.name + '\'');
-    //     return;
-    //   }
-    //
-    //   if (place.geometry.viewport) {
-    //     map.fitBounds(place.geometry.viewport);
-    //
-    //   } else {
-    //     map.setCenter(place.geometry.location);
-    //     map.setZoom(17);
-    //   }
-    //
-    //   startRouteMarker.setPosition(place.geometry.location);
-    //   startRouteMarker.setVisible(true);
-    //
-    //   if (startRouteMarker != null && finishRouteMarker != null) {
-    //     routeTrip();
-    //   }
-    //
-    //   google.maps.event.addListener(startRouteMarker, 'dragend', function () {
-    //     geoCoder.geocode({'latLng': startRouteMarker.getPosition()}, function (results, status) {
-    //       if (status == google.maps.GeocoderStatus.OK) {
-    //         if (results[0]) {
-    //           $('#address-input').val(results[0].formatted_address);
-    //         }
-    //       }
-    //     });
-    //   });
-    // });
+    this.map = new google.maps.Map(this.gMapElement.nativeElement, mapOptions);
+  }
+
+  initPlaces() {
+    // const autocompleteOptions: google.maps.places.AutocompleteOptions = {fields: ['place_id', 'name', 'types', '']};
+    // this.originAutocomplete = new google.maps.places.Autocomplete(this.startAddress.nativeElement, autocompleteOptions);
+    //  this.destinationAutocomplete = new google.maps.places.Autocomplete(this.destinationAddress.nativeElement, autocompleteOptions);
+
+    this.startAddressAutocomplete = new google.maps.places.Autocomplete(this.startAddress.nativeElement);
+    this.endAddressAutocomplete = new google.maps.places.Autocomplete(this.endAddress.nativeElement);
+
+    this.startAddressAutocomplete.bindTo('bounds', this.map);
+
+    google.maps.event.addListener(this.startAddressAutocomplete, 'place_changed', () => {
+      if (!this.startRouteMarker) {
+        this.startRouteMarker = new google.maps.Marker({
+          map: this.map,
+          draggable: true
+        });
+      }
+
+      const place = this.startAddressAutocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert('No details available for input: ' + place.name);
+        return;
+      }
+
+      if (place.geometry.viewport) {
+        this.map.fitBounds(place.geometry.viewport);
+
+      } else {
+        this.map.setCenter(place.geometry.location);
+        this.map.setZoom(17);
+      }
+
+      this.startRouteMarker.setPosition(place.geometry.location);
+      this.startRouteMarker.setVisible(true);
+
+
+      console.log(this.startRouteMarker);
+      console.log(this.startAddressAutocomplete.getPlace().formatted_address);
+      // this.dataService.setSource(this.originAutocomplete.getPlace().formatted_address);
+    });
+
+    google.maps.event.addListener(this.endAddressAutocomplete, 'place_changed', () => {
+      if (!this.endRouteMarker) {
+        this.endRouteMarker = new google.maps.Marker({
+          map: this.map,
+          draggable: true
+        });
+      }
+
+      const place = this.endAddressAutocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert('No details available for input: ' + place.name);
+        return;
+      }
+
+      if (place.geometry.viewport) {
+        this.map.fitBounds(place.geometry.viewport);
+
+      } else {
+        this.map.setCenter(place.geometry.location);
+        this.map.setZoom(17);
+      }
+
+      this.endRouteMarker.setPosition(place.geometry.location);
+      this.endRouteMarker.setVisible(true);
+
+
+      console.log(this.endRouteMarker);
+      console.log(this.endAddressAutocomplete.getPlace().formatted_address);
+      // this.dataService.setSource(this.originAutocomplete.getPlace().formatted_address);
+    });
+
 
   }
 
-  onKeyUp(value) {
+  onKeyUpStartAddress(value) {
     this.startAddress = value;
+  }
+
+  onKeyUpEndAddress(value) {
+    this.endAddress = value;
   }
 }
 
