65af8ca4dadabdf220fa86cb71d80b700384c87d
User <chernyshov.viacheslav@gmail.com>
2020-06-27 16:43:14 +0300

    + route trip between markers

--- src/app/system/driver-page/driver-page.component.ts
+++ src/app/system/driver-page/driver-page.component.ts
@@ -10,6 +10,7 @@ import {FormControl} from '@angular/forms';
 })
 
 export class DriverPageComponent implements AfterContentInit {
+
   @ViewChild('googleMap', {static: true}) public gMapElement: ElementRef;
 
   @ViewChild('startAddress', {static: true}) startAddress: ElementRef;
@@ -27,8 +28,18 @@ export class DriverPageComponent implements AfterContentInit {
   startAddressAutocomplete: google.maps.places.Autocomplete;
   endAddressAutocomplete: google.maps.places.Autocomplete;
 
+  geoCoder: google.maps.Geocoder;
+
+  startAddressInput: string;
+  endAddressInput: string;
+
+  directionsDisplay: google.maps.DirectionsRenderer;
+  waypoints: [];
+  directionsService: google.maps.DirectionsService;
+
   public ngAfterContentInit(): void {
     this.searchControl = new FormControl();
+    this.geoCoder = new google.maps.Geocoder();
 
     this.initMap();
     this.initPlaces();
@@ -56,10 +67,7 @@ export class DriverPageComponent implements AfterContentInit {
 
     google.maps.event.addListener(this.startAddressAutocomplete, 'place_changed', () => {
       if (!this.startRouteMarker) {
-        this.startRouteMarker = new google.maps.Marker({
-          map: this.map,
-          draggable: true
-        });
+        this.startRouteMarker = new google.maps.Marker({map: this.map, draggable: true});
       }
 
       const place = this.startAddressAutocomplete.getPlace();
@@ -79,18 +87,29 @@ export class DriverPageComponent implements AfterContentInit {
       this.startRouteMarker.setPosition(place.geometry.location);
       this.startRouteMarker.setVisible(true);
 
+      if (this.startRouteMarker != null && this.endRouteMarker != null) {
+        this.routeTrip();
+      }
 
-      console.log(this.startRouteMarker);
-      console.log(this.startAddressAutocomplete.getPlace().formatted_address);
-      // this.dataService.setSource(this.originAutocomplete.getPlace().formatted_address);
+      google.maps.event.addListener(this.startRouteMarker, 'dragend', () => {
+        this.geoCoder = new google.maps.Geocoder();
+        this.geoCoder.geocode({location: this.startRouteMarker.getPosition()}, (results, status) => {
+          if (status === google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
+              this.startAddressInput = results[0].formatted_address;
+              // if (this.startRouteMarker != null && this.endRouteMarker != null) {
+              //   console.log('2');
+              //   this.routeTrip();
+              // }
+            }
+          }
+        });
+      });
     });
 
     google.maps.event.addListener(this.endAddressAutocomplete, 'place_changed', () => {
       if (!this.endRouteMarker) {
-        this.endRouteMarker = new google.maps.Marker({
-          map: this.map,
-          draggable: true
-        });
+        this.endRouteMarker = new google.maps.Marker({map: this.map, draggable: true});
       }
 
       const place = this.endAddressAutocomplete.getPlace();
@@ -110,13 +129,60 @@ export class DriverPageComponent implements AfterContentInit {
       this.endRouteMarker.setPosition(place.geometry.location);
       this.endRouteMarker.setVisible(true);
 
+      if (this.startRouteMarker != null && this.endRouteMarker != null) {
+        this.routeTrip();
+      }
 
-      console.log(this.endRouteMarker);
-      console.log(this.endAddressAutocomplete.getPlace().formatted_address);
-      // this.dataService.setSource(this.originAutocomplete.getPlace().formatted_address);
+      google.maps.event.addListener(this.endRouteMarker, 'dragend', () => {
+        this.geoCoder = new google.maps.Geocoder();
+        this.geoCoder.geocode({location: this.endRouteMarker.getPosition()}, (results, status) => {
+          if (status === google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
+              this.endAddressInput = results[0].formatted_address;
+              // if (this.startRouteMarker != null && this.endRouteMarker != null) {
+              //   console.log('4');
+              //   this.routeTrip();
+              // }
+            }
+          }
+        });
+      });
+    });
+  }
+
+
+  routeTrip() {
+    if (this.directionsDisplay != null) {
+      this.directionsDisplay.setMap(null);
+    }
+
+    const rendererOptions = {map: this.map, draggable: true};
+    this.directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+
+    this.directionsDisplay.addListener('directions_changed', () => {
+      // alert(directionsDisplay.directions.routes[0].legs[0].end_location.lat());
+      // alert(startRouteMarker.getPosition().lat());
+      // computeTotalDistance(directionsDisplay.getDirections());
     });
 
+    const request = {
+      origin: this.startRouteMarker.getPosition(),
+      destination: this.endRouteMarker.getPosition(),
+      waypoints: this.waypoints,
+      provideRouteAlternatives: true,
+      travelMode: google.maps.TravelMode.DRIVING
+    };
 
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsService.route(request, (response, status) => {
+      if (status === google.maps.DirectionsStatus.OK) {
+        this.directionsDisplay.setDirections(response);
+      } else {
+        alert('failed to get directions');
+      }
+    });
+    this.startRouteMarker.setVisible(false);
+    this.endRouteMarker.setVisible(false);
   }
 
   onKeyUpStartAddress(value) {
